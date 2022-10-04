88ad0917bebbe19cb6fc77ef7991f328df3e9ff6
Robby <77552033+TinyRobby@users.noreply.github.com>
2022-10-03 20:08:38 -0400

    Update main.cpp

--- Git-PKG/main.cpp
+++ Git-PKG/main.cpp
@@ -1,20 +1,30 @@
-#include <Windows.h>
 #include <iostream>
 #include <cstdlib>
 #include <fstream>
 #include "Headers/json.hpp"
+#include <curl/curl.h>
 
 using namespace std;
-using namespace nlohmann;
+using json = nlohmann::json;
 
-int main( int argc, char *argv[] )
+int main(int argc, char *argv[])
 {
     int choice;
-    cout << "Because of my lack of programming skills, I am not able to use arguments yet.\nSo just choose from the following options.\n1)Install a program\n2)Remove a program\n3)Get developer options.\nMore choices coming soon!" << endl;
+    string pkg;
+    ofstream out;
+    CURL *handle = curl_easy_init();
+    if(!handle) { cout << "cURL didn't load" << endl; return 1; }
+    cout << "Because of my lack of programming skills or my IDE/OS being bad, I am not able to use arguments yet.\nSo just choose from the following options.\n1)Install a program\n2)Remove a program\n3)Get developer options.\nMore choices coming soon!" << endl;
     cout << "Make your selection now. Numbers only Plz!" << endl;
     cin >> choice;
     if(choice == 1) {
-        cout << "This feature is still under developement. Please wait." << endl;
+        cout << "Program name: ";
+        cin >> pkg;
+        CURLcode res;
+        string ting
+        curl_easy_setopt(handle, CURLOPT_URL, "https://google.com/");
+        res = curl_easy_perform(handle);
+        curl_easy_cleanup(handle);
         return 0;
     }
     if(choice == 2) {
@@ -22,9 +32,13 @@ int main( int argc, char *argv[] )
         return 0;
     }
     if(choice == 3) {
-        cout << "Welcome to the developer options.\nSome tools are required going forward. Find them here:\nhttps://tinyrobby.github.io/gitpkg\nAll you need to do is click the link and pres Ctrl+S and follow the instructions on how to build a json package file."  << endl;
-
+        cout << "Welcome to the developer options.\nSome tools are required going forward. The tools can be found if you type \'docs\' after restarting the program  Find them here:\nhttps://tinyrobby.github.io/gitpkg\nAll you need to do is click the link and pres Ctrl+S and follow the instructions on how to build a json package file."  << endl;
     }
-
+    else {
+        cout << "Nope." << endl;
+    }
+    //json
     return 0;
+
 }
+
